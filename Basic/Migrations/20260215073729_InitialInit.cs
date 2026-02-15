99ab53bb8e0a2756243ebab6fa031cba92ae855a
Ahmet Kaya <academyakm0@gmail.com>
2026-02-15 10:43:44 +0300

    Migrations

---
+++ Basic/Migrations/20260215073729_InitialInit.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Basic.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BootcampKayit",
+                columns: table => new
+                {
+                    KayitId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OgrenciId = table.Column<int>(type: "INTEGER", nullable: false),
+                    BootcampId = table.Column<int>(type: "INTEGER", nullable: false),
+                    KayitTarihi = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BootcampKayit", x => x.KayitId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bootcamps",
+                columns: table => new
+                {
+                    BootcampId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BootcampName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bootcamps", x => x.BootcampId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ogrenciler",
+                columns: table => new
+                {
+                    OgrenciId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OgrenciAd = table.Column<string>(type: "TEXT", nullable: true),
+                    OgrenciSoyad = table.Column<string>(type: "TEXT", nullable: true),
+                    Image = table.Column<string>(type: "TEXT", nullable: true),
+                    Telefon = table.Column<string>(type: "TEXT", nullable: true),
+                    Eposta = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ogrenciler", x => x.OgrenciId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BootcampKayit");
+
+            migrationBuilder.DropTable(
+                name: "Bootcamps");
+
+            migrationBuilder.DropTable(
+                name: "Ogrenciler");
+        }
+    }
+}
--- Basic/obj/Debug/net9.0/Basic.AssemblyInfo.cs
+++ Basic/obj/Debug/net9.0/Basic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd3348df7bd161e244a0fbeb22c3ab7b9fd0c5c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e7cf063ae0be6327de6168f8bb619923885c5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
